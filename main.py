88d691c09aa161374f37ad440bf0bd3b07812410
lotar69 <lotardy@gmail.com>
2022-02-18 16:53:58 +0100

    secure

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 from pprint import pprint
 
-API_Key = "257eb9b5d06997d2c288543f8e97d165"
+API_Key = input("Enter your open weather map API Key:")
 
 city = input("Enter a city:\n")
 
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+str(API_Key)+"&q="+city
 
 weather_data = requests.get(base_url).json()
 
